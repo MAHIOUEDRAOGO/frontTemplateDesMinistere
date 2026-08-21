da2dceb3261dc971cf95e3f3bd59b0c87c4e9ec7
thionnais <lethionnaisabel@gmail.com>
2026-08-21 14:09:20 +0000

    vendredi 210-08-2026 fin des modif apporte au titre 'ressources' 14h10

---
+++ src/app/features/admin/ressources/mediatheques/mediatheques.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mediatheques',
+  imports: [],
+  templateUrl: './mediatheques.component.html',
+  styleUrl: './mediatheques.component.scss',
+})
+export class MediathequesComponent {
+
+}
